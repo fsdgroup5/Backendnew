cdc2153327630df7e6bded2bbadf205880e4add6
fsdgroup5 <109898223+fsdgroup5@users.noreply.github.com>
2022-08-07 15:05:52 +0530

    Update UserLoginRouter.js

--- src/routes/UserLoginRouter.js
+++ src/routes/UserLoginRouter.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const UserLoginRouter = express.Router();
-const UserData = require("../model/users");
+const UserData = require("../model/Employee");
 //UserLoginRouter.use(express.urlencoded({extended:true}));
 //UserLoginRouter.use(express.json());
 const jwt = require('jsonwebtoken')
 
 UserLoginRouter.post('', (req, res) => {
     let userData1 = req.body
-    UserData.findOne({"username":userData1.username,"password":userData1.password}).then((data)=>{
+    UserData.findOne({"username":userData1.userid,"password":userData1.password}).then((data)=>{
      console.log(data)
      if (data===null) {
        res.status(401).send('Invalid Username and password!!')
-     } else if(data.username===userData1.username && data.password===userData1.password){
-       let payload = {subject: userData1.username+userData1.password}
+     } else if(data.userid===userData1.userid && data.password===userData1.password){
+       let payload = {subject: userData1.userid+userData1.password}
        let token = jwt.sign(payload, 'secretKey')
        res.status(200).send({token})
      }
